a9dbf53490df95cc6dcba0315c5b7657970cb852
R.J. Steinert <rj@rjsteinert.com>
2015-05-25 20:16:24 -0400

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,3 +1,3 @@
-#! bin/sh
+#! /bin/sh
 
 watch -n{{number}} "./opk-temper1-cli/pull | ./opk-phant-cli/push --url {{phant-url}} --public_key {{phant-public-key}} --private_key {{phant-private-key}} --field_name {{phant-field-name}}"
